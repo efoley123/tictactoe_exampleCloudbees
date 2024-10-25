cad01cbce6bb92b212246b81fbcd5ae1fbabeebe
Rosy122333 <80847580+Rosy122333@users.noreply.github.com>
2024-10-25 10:23:04 -0700

    new_test

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -8,3 +8,9 @@ def test_tic_tac_toe():
     game.make_move(1, 'X')
     game.make_move(2, 'X')
     assert game.current_winner == 'X'  # X wins
+
+def test_tic_tac_toe2():
+    game2 = TicTacToe()
+    assert game2.make_move(0, 'X') == True
+    assert game2.make_move(1, 'O') == True
+    assert game2.current_winner is None
